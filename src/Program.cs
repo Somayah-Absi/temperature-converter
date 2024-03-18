a6700b0bba9f8231810440dc585deedcced88be3
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-03-18 13:09:09 +0300

    starting

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static string TempConvert (int x, string y)
+    {
+          int converter=0;
+        string newTemp="";
+    if(y=="c"){
+converter=(x*9/5)+32;
+newTemp="f";
+        
+    }else if(y=="f"){
+        converter=(x-32)*9/5;
+newTemp="c";
+    }
+    return $"{converter} {newTemp}";
+    }
+    
+
+    public static void Main(string[] args)
+    {
+       
+        
+        Console.WriteLine(TempConvert(100,"c"));
+
+       
+        
+    }
+}
